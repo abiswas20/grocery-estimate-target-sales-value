2adf6f5e3d5058d82fbc3b0403059acb336c5dc0
apratim biswas <apratimb157@gmail.com>
2021-03-24 09:48:14 +0530

    first commit

---
+++ Dunnhumby - The Complete Journey - Deployment/Campaign - Single Parent Household - Liquid Milk/campaign_liquid_milk.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from sklearn.externals import joblib
+import numpy as np
+
+
+CAMPAIGN_LIQUID_MILK=FLASK(__name__)
+API = Api(CAMPAIGN_LIQUID_MILK)
+
+
+
