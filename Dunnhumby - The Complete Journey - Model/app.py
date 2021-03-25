1dd271bf40a21fb35b3c08d1f6c2c7114e5d3d60
apratim biswas <apratimb157@gmail.com>
2021-03-25 15:45:43 +0530

    app v3.25

--- Dunnhumby - The Complete Journey - Model/app.py
+++ Dunnhumby - The Complete Journey - Model/app.py
@@ -31,11 +31,11 @@ def func_man_id(m_p):
 retail_disc = float(st.text_input("retail discount",0.05))
 coupon_disc = float(st.text_input("coupon discount",0.01))
 coupon_match_disc = float(st.text_input("coupon match discount",0))
-day = int(st.text_input("day", 0))
-volume = int(st.text_input("volume",128))
-brand = st.selectbox("private brand?",(0,1))
-fluid_white_milk=st.selectbox("FLUID MILK WHITE ONLY",(0,1))
-miscellaneous_milk=st.selectbox("MISCELLANEOUS MILK",(0,1))
+day = st.slider("number of days since the start of the marketing campaign", min_value=1,max_value=1000)
+volume = st.selectbox("unit volume (fl oz)",(128,64,32,16))
+brand = st.selectbox("private brand",(0,1))
+fluid_white_milk=st.selectbox("fluid milk, white only",(0,1))
+miscellaneous_milk=st.selectbox("miscellaneous milk",(0,1))
 plant=func_man_id(st.selectbox("manufacturer id",('1543', '926', '1078', '927', '5423', '1487', '1453', '531', '865','709')))
 user_input = [retail_disc, coupon_disc, coupon_match_disc, day, volume, brand, fluid_white_milk, miscellaneous_milk]
 for i in plant:
@@ -44,12 +44,12 @@ user_input=np.array(user_input)
 user_input=user_input.reshape(1,-1)
 scaler = load('scaler_file.save')
 user_input_scaled=scaler.transform(user_input)
-st.write(user_input_scaled)
+#st.write(user_input_scaled) #to debug input
 ############
 
-with open("spf_model.joblib", "rb") as f:
+with open("sales_value_lasso_model.joblib", "rb") as f:
     clf = load(f)
     # make predictions
     prediction = clf.predict(user_input_scaled)
-    st.header(f"The model predicts: {prediction[0]}")
+    st.header(f"Prediction: ${prediction[0]:.2f} sales/transaction after discounts.")
 
